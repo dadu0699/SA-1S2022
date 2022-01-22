a3314ab7a5a0710e9d61f6a5953b03b44f752b72
dadu0699 <dadu0699@hotmail.com>
2022-01-21 21:19:32 -0600

    [ADD] Log helper

---
+++ Practica_1/customer-service/helpers/logHandler.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+const { v4: uuidv4 } = require('uuid');
+
+const writeLog = (log) => {
+  const date = new Date();
+  const timestamp = date.toISOString();
+
+  const logPath = path.resolve('public/logs.json');
+  const logObj = {
+    id: uuidv4(),
+    timestamp,
+    ...log,
+  };
+
+  fs.readFile(logPath, (err, data) => {
+    let logs = [];
+    if (!err) logs = JSON.parse(data);
+    logs.push(logObj);
+
+    fs.writeFile(logPath, JSON.stringify(logs), (err) => {
+      if (err) console.log(err);
+    });
+  });
+};
+
+module.exports = { writeLog };
--- Practica_1/login-service/app.js
+++ Practica_1/login-service/app.js
@@ -19,6 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/', require('./routes/index.route'));
+// app.use('/logs', express.static(path.resolve('logs')));
 
 // Catch 404
 app.use(require('./middlewares/notFound'));
