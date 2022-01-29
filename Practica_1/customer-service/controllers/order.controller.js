edddbb6859114e1689c1d93242bc68ce137de346
dadu0699 <dadu0699@hotmail.com>
2022-01-29 00:17:47 -0600

    [UDPATE] Ordering functionality

--- Practica_1/customer-service/controllers/order.controller.js
+++ Practica_1/customer-service/controllers/order.controller.js
@@ -48,4 +48,71 @@ const requestOrder = async (req, res) => {
   }
 };
 
-module.exports = { requestOrder };
+const getStatus = (req, res) => {
+  const { orderID } = req.query;
+  const status = req.status;
+  const { id: userID, username, role } = req.user;
+
+  if (role !== 'CUSTOMER') {
+    writeLog({
+      username,
+      action: 'request order',
+      status: 'failed',
+      message: 'User is not a customer',
+    });
+    res.status(401).send({ code: 401, data: 'Unauthorized' });
+  }
+
+  try {
+    const content = fs.readFileSync(logPath, 'utf8');
+    const data = JSON.parse(content);
+    const order = data.find(
+      (order) =>
+        order.order_id === parseInt(orderID, 10) && order.user.id === userID
+    );
+
+    if (!order) {
+      writeLog({
+        username,
+        action: 'request order',
+        status: 'failed',
+        message: 'Order not found',
+      });
+      return res.status(404).send({ code: 404, data: 'Order not found' });
+    }
+
+    writeLog({
+      username,
+      action: 'request order',
+      status: 'success',
+      message: `Order status at verified ${status ? 'restaurant' : 'delivery'}`,
+    });
+
+    const { products, restaurant_order_status, delivery_order_status } = order;
+    let payload = {};
+
+    console.log(status);
+    if (status) {
+      payload = { products, restaurant_order_status };
+    } else {
+      payload = { products, delivery_order_status };
+    }
+
+    res.status(200).send({ code: 200, data: payload });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ code: 500, data: err });
+  }
+};
+
+const restaurantStatus = (req, res) => {
+  req.status = true;
+  getStatus(req, res);
+};
+
+const deliveryStatus = (req, res) => {
+  req.status = false;
+  getStatus(req, res);
+};
+
+module.exports = { requestOrder, restaurantStatus, deliveryStatus };
--- Practica_1/customer-service/routes/order.route.js
+++ Practica_1/customer-service/routes/order.route.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const { requestOrder } = require('../controllers/order.controller');
+const {
+  requestOrder,
+  restaurantStatus,
+  deliveryStatus,
+} = require('../controllers/order.controller');
 const validateToken = require('../middlewares/validateToken');
 
 router.route('/').post(validateToken, requestOrder);
+router.route('/restaurant-status').get(validateToken, restaurantStatus);
+router.route('/delivery-status').get(validateToken, deliveryStatus);
 
 module.exports = router;
