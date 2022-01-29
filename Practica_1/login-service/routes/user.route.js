77071979a4cb38274de337a6ae67d58bf64a2e8f
dadu0699 <dadu0699@hotmail.com>
2022-01-28 20:06:00 -0600

    [UPDATE] Passwords route

--- Practica_1/login-service/routes/user.route.js
+++ Practica_1/login-service/routes/user.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { encryptPasswords, signin } = require('../controllers/user.controller');
 
-router.route('/encryptPasswords').get(encryptPasswords);
+router.route('/encrypt-passwords').get(encryptPasswords);
 router.route('/sign-in').post(signin);
 
 module.exports = router;
