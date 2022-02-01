56d0c02a85d6bddf71f499b81069de1ce9888dbe
María Reneé Juárez Albizures <debymrj@gmail.com>
2022-02-01 06:13:54 -0600

    [ADD] order status

--- Practica_1/restaurant-service/controllers/restaurant.controller.js
+++ Practica_1/restaurant-service/controllers/restaurant.controller.js
@@ -31,14 +31,14 @@ const receiveOrder = async (req, res) => {
             (order) => order.order_id === orderID
         );
 
-        if(order){
+        if (order) {
             writeLog({
                 username,
                 action: 'received order',
                 status: 'failed',
                 message: 'Order already received',
-              });
-              return res.status(404).send({ code: 404, data: 'Order already received' });
+            });
+            return res.status(404).send({ code: 404, data: 'Order already received' });
         }
 
         const newOrder = {
@@ -60,6 +60,81 @@ const receiveOrder = async (req, res) => {
         console.error(err);
         res.status(500).send({ code: 500, data: err });
     }
+
 };
 
-module.exports = { receiveOrder };
+const setStatus = (req, res) => {
+    const { orderID, newStatus } = req.query;
+    const status = req.status;
+    const { id: userID, username, role } = req.user;
+
+    if (role !== 'RESTAURANT') {
+        writeLog({
+            username,
+            action: 'change order status',
+            status: 'failed',
+            message: 'User is not a restaurant',
+        });
+        res.status(401).send({ code: 401, data: 'Unauthorized' });
+    }
+
+    try {
+        const content = fs.readFileSync(filePath, 'utf8');
+        const data = JSON.parse(content);
+
+
+        const restaurant = data.find(
+            (restaurant) =>
+                restaurant.restaurant_id === parseInt(userID, 10)
+        );
+
+        const order = restaurant.orders.find(
+            (order) => order.order_id === parseInt(orderID, 10)
+        );
+
+        if (!order) {
+            writeLog({
+                username,
+                action: 'change order status',
+                status: 'failed',
+                message: 'Order not found',
+            });
+            return res.status(404).send({ code: 404, data: 'Order not found' });
+        }
+
+
+        console.log(status);
+        if (status) {
+            order.client_order_status = orderStatus[newStatus];
+        } else {
+            order.delivery_order_status = orderStatus[9];
+        }
+
+        fs.writeFileSync(filePath, JSON.stringify(data));
+
+
+        writeLog({
+            username,
+            action: 'change order status',
+            status: 'success',
+            message: `Order ${status ? 'status updated' : 'ready for pick up'}`,
+        });
+
+        res.status(200).send({ code: 200, data: order });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send({ code: 500, data: err });
+    }
+};
+
+const clientStatus = (req, res) => {
+    req.status = true;
+    setStatus(req, res);
+};
+
+const deliveryStatus = (req, res) => {
+    req.status = false;
+    setStatus(req, res);
+};
+
+module.exports = { receiveOrder, clientStatus, deliveryStatus };
--- Practica_1/restaurant-service/routes/restaurant.route.js
+++ Practica_1/restaurant-service/routes/restaurant.route.js
@@ -3,10 +3,13 @@ const router = express.Router();
 
 const {
   receiveOrder,
+  clientStatus,
+  deliveryStatus,
 } = require('../controllers/restaurant.controller');
 const validateToken = require('../middlewares/validateToken');
 
 router.route('/').post(validateToken, receiveOrder);
-
+router.route('/client-status').get(validateToken, clientStatus);
+router.route('/delivery-status').get(validateToken, deliveryStatus);
 
 module.exports = router;
